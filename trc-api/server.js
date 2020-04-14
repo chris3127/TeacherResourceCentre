ceacfb26dccfc0008919187c7dcf00d9a28647ae
Chris Moran <chrismoran3127@gmail.com>
2020-04-14 17:15:44 +1000

    removed cors for heroku

--- trc-api/server.js
+++ trc-api/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
